b5ab174341ba531964023a5561b68af4cbb4147a
jayeshk-star <jayeshkale052@gmail.com>
2021-07-21 09:53:31 +0530

    start

--- main/src/App.js
+++ main/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+// import Iamge from "./iamge/Iamge";
+// import Slider from "./iamge/Slider";
 
 function App() {
-  return <div></div>;
+  return <div>
+    
+    new project
+  </div>;
 }
 
 export default App;
