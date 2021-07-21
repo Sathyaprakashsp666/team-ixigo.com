d1267787c9c764d59ce23cf5765a8a77f83fc137
jayeshk-star <jayeshkale052@gmail.com>
2021-07-21 10:25:39 +0530

    middle part start

---
+++ main/src/Components/MiddlePart/MiddlePart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BusDestination from './BusDestination'
+
+const MiddlePart = () => {
+    return (
+        <div>
+            <BusDestination />
+        </div>
+    )
+}
+
+export default MiddlePart
