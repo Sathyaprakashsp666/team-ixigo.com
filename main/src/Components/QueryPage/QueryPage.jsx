8f3d451d7324e950c5ce13dd6d779e305da1ab6e
Rohit Rawat <rohitrawat4242@gmail.com>
2021-07-24 15:49:11 +0530

    filter card UI made

--- main/src/Components/QueryPage/QueryPage.jsx
+++ main/src/Components/QueryPage/QueryPage.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux';
 import { BusCard } from './BusCard';
+import { FilterCard } from './FilterCard';
 import { NavBar } from './NavBar';
 export const QueryPage = () => {
   const data = useSelector((state) => state.busReducer);
   return (
     <div style={{ background: '#f2f2f2' }}>
         <NavBar/>
+        <FilterCard />
       {data.isLoading ? <h2>...Loading</h2> : data.routeData?.buses?.map(data=> <BusCard data={data}/>) }
     </div>
   );
